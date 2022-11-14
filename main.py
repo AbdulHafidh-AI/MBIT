403fbacf79f635c43bd1d6d39a3ab7668d139317
AbdulHafidh-AI <abdul_h20@mhs.unsyiah.ac.id>
2022-11-14 20:47:04 +0700

    setup Artificial Inteligence Project to make web-based expert system

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split 
+
+st.write("""
+    # Sistem pakar dalam menentukan minat bakat mahasiswa informatika
+""")
+
+st.image('./images.png')
+
+
+ 
+
+
