7b72c1caf7cd87cf60a4dd86c249fd475c1fb035
Haris Daffa <79704418+harisde03@users.noreply.github.com>
2022-11-21 23:19:38 +0700

    Add Fake Data

---
+++ data/faker.py
@@ -0,0 +1,43 @@
+# 1. Timestamp
+# 2. DM
+# 3. DM
+# 4. DM
+# 5. DM
+# 6. RPL
+# 7. RPL
+# 8. RPL
+# 9. RPL
+# 10. Jaringan
+# 11. Class
+# 12. Jaringan
+# 13. GIS
+# 14. Jaringan
+# 15. Jaringan
+# 16. KOSONG
+# 17. GIS
+# 18. GIS
+
+import random
+from datetime import date
+
+dataMining = []
+
+for i in range(100):
+	print("21/11/2022 22:00:00,", end="")
+	print("Setuju," if random.randint(0, 1) == 1 else "Tidak Setuju,", end="")
+	print("Setuju," if random.randint(0, 1) == 1 else "Tidak Setuju,", end="")
+	print("Setuju," if random.randint(0, 1) == 1 else "Tidak Setuju,", end="")
+	print("Setuju," if random.randint(0, 1) == 1 else "Tidak Setuju,", end="")
+	print("Setuju," if random.randint(0, 1) == 1 else "Tidak Setuju,", end="")
+	print("Setuju," if random.randint(0, 1) == 1 else "Tidak Setuju,", end="")
+	print("Setuju," if random.randint(0, 1) == 1 else "Tidak Setuju,", end="")
+	print("Setuju," if random.randint(0, 1) == 1 else "Tidak Setuju,", end="")
+	print("Setuju," if random.randint(0, 1) == 1 else "Tidak Setuju,", end="")
+	print("Geografis Information System,", end="")
+	print("Setuju," if random.randint(0, 1) == 1 else "Tidak Setuju,", end="")
+	print("Setuju,", end="")
+	print("Setuju," if random.randint(0, 1) == 1 else "Tidak Setuju,", end="")
+	print("Setuju," if random.randint(0, 1) == 1 else "Tidak Setuju,", end="")
+	print(",", end="")
+	print("Setuju,", end="")
+	print("Setuju")
