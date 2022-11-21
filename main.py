703e392dc36f2a88400177fd85a19de5ca8de1ab
Haris Daffa <79704418+harisde03@users.noreply.github.com>
2022-11-21 22:31:26 +0700

    Rapiin main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pickle
 st.image('./assets/images.png')
 
 st.write("""
-    # Sistem pakar dalam menentukan minat bakat mahasiswa informatika
+    # Sistem Pakar dalam Menentukan Minat Bakat Mahasiswa Informatika
 """)
 
 
@@ -61,51 +61,43 @@ st.write("""
     - Apakah kamu pernah membayangkan diri kamu itu sebagai seorang cyber security?
     - Apakah kamu suka dalam mengolah suatu citra/gambar?
     - Apakah kamu suka menjelajah suatu wilayah dengan tujuan untuk meneliti suatu wilayah?
-    
-
 """)
 
 # get user input from web
-soal1 = st.selectbox('Apakah kamu tertarik dengan matematika?', ('Setuju', 'Tidak Setuju'))
-soal2 = st.selectbox('Apakah kamu tertarik dengan bidang keilmuan statistika?', ('Setuju', 'Tidak Setuju'))
-soal3 = st.selectbox('Apakah kamu suka melihat data-data yang sangat banyak?', ('Setuju', 'Tidak Setuju'))
-soal4 = st.selectbox('Apakah kamu suka dalam hal menerawang sesuatu?', ('Setuju', 'Tidak Setuju'))
-soal5 = st.selectbox('Apakah kamu suka merancang atau mendesain sesuatu?', ('Setuju', 'Tidak Setuju'))
-soal6 = st.selectbox('Apakah kamu suka membikin aplikasi mobile?', ('Setuju', 'Tidak Setuju'))
-soal7 = st.selectbox('Apakah kamu tertarik dalam hal membikin game?', ('Setuju', 'Tidak Setuju'))
-soal8 = st.selectbox('Apakah kamu tertarik dalam hal bikin web?', ('Setuju', 'Tidak Setuju'))
-soal9 = st.selectbox('Apakah kamu tertarik dalam hal hacking seperti yang ada dalam film?', ('Setuju', 'Tidak Setuju'))
-soal10 = st.selectbox('Apakah kamu tertarik dalam hal membobol situs web seperti bjorka?', ('Setuju', 'Tidak Setuju'))
-soal11 = st.selectbox('Apakah kamu suka dalam hal pemetaan?', ('Setuju', 'Tidak Setuju'))
-soal12 = st.selectbox('Apakah kamu suka membajak komputer orang?', ('Setuju', 'Tidak Setuju'))
-soal13 = st.selectbox('Apakah kamu pernah membayangkan diri kamu itu sebagai seorang cyber security?', ('Setuju', 'Tidak Setuju'))
-
-soal14 = st.selectbox('Apakah kamu suka dalam mengolah suatu citra/gambar?', ('Setuju', 'Tidak Setuju'))
-soal15 = st.selectbox('Apakah kamu suka menjelajah suatu wilayah dengan tujuan untuk meneliti suatu wilayah ?', ('Setuju', 'Tidak Setuju'))
-
-
-
-
-
+soal1 = st.selectbox('Apakah kamu tertarik dengan matematika?', ('Tidak Setuju', 'Setuju'))
+soal2 = st.selectbox('Apakah kamu tertarik dengan bidang keilmuan statistika?', ('Tidak Setuju', 'Setuju'))
+soal3 = st.selectbox('Apakah kamu suka melihat data-data yang sangat banyak?', ('Tidak Setuju', 'Setuju'))
+soal4 = st.selectbox('Apakah kamu suka dalam hal menerawang sesuatu?', ('Tidak Setuju', 'Setuju'))
+soal5 = st.selectbox('Apakah kamu suka merancang atau mendesain sesuatu?', ('Tidak Setuju', 'Setuju'))
+soal6 = st.selectbox('Apakah kamu suka membikin aplikasi mobile?', ('Tidak Setuju', 'Setuju'))
+soal7 = st.selectbox('Apakah kamu tertarik dalam hal membikin game?', ('Tidak Setuju', 'Setuju'))
+soal8 = st.selectbox('Apakah kamu tertarik dalam hal bikin web?', ('Tidak Setuju', 'Setuju'))
+soal9 = st.selectbox('Apakah kamu tertarik dalam hal hacking seperti yang ada dalam film?', ('Tidak Setuju', 'Setuju'))
+soal10 = st.selectbox('Apakah kamu tertarik dalam hal membobol situs web seperti bjorka?', ('Tidak Setuju', 'Setuju'))
+soal11 = st.selectbox('Apakah kamu suka dalam hal pemetaan?', ('Tidak Setuju', 'Setuju'))
+soal12 = st.selectbox('Apakah kamu suka membajak komputer orang?', ('Tidak Setuju', 'Setuju'))
+soal13 = st.selectbox('Apakah kamu pernah membayangkan diri kamu itu sebagai seorang cyber security?', ('Tidak Setuju', 'Setuju'))
+soal14 = st.selectbox('Apakah kamu suka dalam mengolah suatu citra/gambar?', ('Tidak Setuju', 'Setuju'))
+soal15 = st.selectbox('Apakah kamu suka menjelajah suatu wilayah dengan tujuan untuk meneliti suatu wilayah ?', ('Tidak Setuju', 'Setuju'))
 
 # create new data from user input and convert value to float
 new_data = {
-    # If value = "Setuju" then convert to 1.0, else convert to 0.0
-    'soal1': 1.0 if soal1 == 'Setuju' else 0.0,
-    'soal2': 1.0 if soal2 == 'Setuju' else 0.0,
-    'soal3': 1.0 if soal3 == 'Setuju' else 0.0,
-    'soal4': 1.0 if soal4 == 'Setuju' else 0.0,
-    'soal5': 1.0 if soal5 == 'Setuju' else 0.0,
-    'soal6': 1.0 if soal6 == 'Setuju' else 0.0,
-    'soal7': 1.0 if soal7 == 'Setuju' else 0.0,
-    'soal8': 1.0 if soal8 == 'Setuju' else 0.0,
-    'soal9': 1.0 if soal9 == 'Setuju' else 0.0,
-    'soal10': 1.0 if soal10 == 'Setuju' else 0.0,
-    'soal11': 1.0 if soal11 == 'Setuju' else 0.0,
-    'soal12': 1.0 if soal12 == 'Setuju' else 0.0,
-    'soal13': 1.0 if soal13 == 'Setuju' else 0.0,
-    'soal14': 1.0 if soal14 == 'Setuju' else 0.0,
-    'soal15': 1.0 if soal15 == 'Setuju' else 0.0,
+    # If value = "Setuju" then convert to 0.0, else convert to 1.0
+    'soal1': 0.0 if soal1 == 'Setuju' else 1.0,
+    'soal2': 0.0 if soal2 == 'Setuju' else 1.0,
+    'soal3': 0.0 if soal3 == 'Setuju' else 1.0,
+    'soal4': 0.0 if soal4 == 'Setuju' else 1.0,
+    'soal5': 0.0 if soal5 == 'Setuju' else 1.0,
+    'soal6': 0.0 if soal6 == 'Setuju' else 1.0,
+    'soal7': 0.0 if soal7 == 'Setuju' else 1.0,
+    'soal8': 0.0 if soal8 == 'Setuju' else 1.0,
+    'soal9': 0.0 if soal9 == 'Setuju' else 1.0,
+    'soal10': 0.0 if soal10 == 'Setuju' else 1.0,
+    'soal11': 0.0 if soal11 == 'Setuju' else 1.0,
+    'soal12': 0.0 if soal12 == 'Setuju' else 1.0,
+    'soal13': 0.0 if soal13 == 'Setuju' else 1.0,
+    'soal14': 0.0 if soal14 == 'Setuju' else 1.0,
+    'soal15': 0.0 if soal15 == 'Setuju' else 1.0,
 }
 
 
